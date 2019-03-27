0bc11451a5a2c7035a02b54365a9ea124fcb1f6f
Kai Persoff <35818117+NoBannerFlags@users.noreply.github.com>
2019-03-27 12:54:55 -0600

    Add scenes & RTS folders

---
+++ RTS/ResourceManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace RTS
+{
+    
+    public static class ResourceManager
+    {
+        //speed modifieres
+        public static float ScrollSpeed { get { return 25; } }
+        public static float RotateSpeed { get { return 100; } }
+
+        //how far away the mouse must be from the border of the screen in order to scroll.
+        public static int ScrollWidth {  get { return 15; } }
+
+
+        //how high the camera can go. limits it to not go above MaxCameraHeight or below MinCameraHeight
+        public static float MinCameraHeight { get { return 10; } }
+        public static float MaxCameraHeight { get { return 40; } }
+    }
+}
